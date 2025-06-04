a7782620ddd6b848c697340a081df004bdb4ca36
Brandon Uriel García Ramos <weareimsoft@gmail.com>
2025-06-03 20:04:56 -0600

    Se agrego sitemap y robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
-
 import solidJs from "@astrojs/solid-js";
+import sitemap, { ChangeFreqEnum } from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://imsoft.io",
+
   vite: {
     plugins: [tailwindcss()],
   },
@@ -17,5 +18,39 @@ export default defineConfig({
     },
   }),
 
-  integrations: [solidJs()],
+  integrations: [
+    solidJs(),
+    sitemap({
+      changefreq: ChangeFreqEnum.DAILY, //
+      priority: 0.8,
+      lastmod: new Date(),
+
+      filter: (url) => {
+        return !/\/404\/?$/.test(url) && !/\/admin\//.test(url);
+      },
+
+      serialize: (item) => {
+        if (/^https:\/\/imsoft\.io\/blog\//.test(item.url)) {
+          return {
+            ...item,
+            lastmod: new Date().toISOString(),
+            changefreq: ChangeFreqEnum.DAILY,
+            priority: 0.9,
+          };
+        }
+
+        if (item.url === "https://imsoft.io/") {
+          return {
+            ...item,
+            lastmod: new Date().toISOString(),
+            changefreq: ChangeFreqEnum.DAILY,
+            priority: 1.0,
+          };
+        }
+
+        return item;
+      },
+
+    }),
+  ],
 });
