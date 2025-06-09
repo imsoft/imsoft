140ffd8caf3082b9f6a2d7c0423572c00d83f5a1
Brandon Uriel García Ramos <weareimsoft@gmail.com>
2025-06-09 15:50:34 -0600

    Se corrigieron colores y se agrego fixed a header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -166,7 +166,7 @@ export default function Header(props: HeaderProps): JSX.Element {
   };
 
   return (
-    <header class="bg-[var(--background)] text-[var(--foreground)]">
+    <header class="w-full fixed top-0 left-0 z-50 bg-[var(--background)] text-[var(--foreground)]">
       <nav
         class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
